a4d66510689bd35dc7cf22e082ba8d46afaee105
dann <gh@mrmuster.com>
2026-03-03 19:56:58 -0500

    set raw

---
+++ daimio/pflavs/dom-set-raw-html.js
@@ -0,0 +1,33 @@
+import D from '../1_daimio.js'
+// TODO: convert these 'set' style ports to use track_event
+
+D.import_port_flavour('dom-set-raw-html', {
+  dir: 'out',
+  unsafe: true,
+  settings: [
+    {
+      key: 'thing',
+      desc: 'A dom selector for binding',
+      type: 'selector'
+    },
+    {
+      key: 'parent',
+      desc: 'A dom element contain thing. Defaults to document.',
+      type: 'id'
+    },
+  ],
+  outside_exit: function(ship) {
+    // OPT: we could save some time by tying this directly to paint events: use requestAnimationFrame and feed it the current ship. that way we skip the layout cost between screen paints for fast moving events.
+    if(this.element)
+      this.element.innerHTML = D.stringify(ship)
+  },
+  outside_add: function() {
+    this.element = document.getElementById(this.settings.thing)
+
+    if(!this.element)
+      return D.set_error('That dom thing ("' + this.settings.thing + '") is not present')
+
+    if(!('innerHTML' in this.element))
+      return D.set_error('That dom thing has no innerHTML')
+  }
+})
